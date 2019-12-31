8e18177a1d071ef549fedfd6fce20abc20b8f6a4
litalz <litalbdbd@gmail.com>
2019-12-31 15:23:05 +0200

    added cloud and rock

--- mineCraft.js
+++ mineCraft.js
@@ -14,6 +14,8 @@ mineCraft.mapArray = [
 mineCraft.start = () => {
     mineCraft.createDiv();
     mineCraft.createGround();
+    mineCraft.createCloud();
+    mineCraft.createRock();
 }
 mineCraft.createDiv = () => {
     for (let i = 0; i < mineCraft.mapArray.length; i++) {
@@ -35,6 +37,24 @@ mineCraft.createGround = () => {
     for (let i = 7; i < 10; i++) {
         for (let j = 0; j < mineCraft.mapArray[i].length; j++) {
             document.querySelector(`#c${i}_${j}`).style.backgroundImage = 'url("./images/dirt.jpg")';
+        }
+    }
+}
+
+mineCraft.createCloud = () => {
+    document.querySelector('#c1_2').style.backgroundColor = 'white';
+    for (let i = 2; i < 3; i++) {
+        for (let j = 1; j < 4; j++) {
+            document.querySelector(`#c${i}_${j}`).style.backgroundColor = 'white';
+        }
+    }
+}
+
+mineCraft.createRock = () => {
+
+    for (let i = 6; i < 7; i++) {
+        for (let j = 5; j < 7; j++) {
+            document.querySelector(`#c${i}_${j}`).style.backgroundImage = 'url("./images/rocks.jpg")';
         }
     }
 }
