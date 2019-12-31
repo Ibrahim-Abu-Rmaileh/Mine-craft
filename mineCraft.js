3c12630074208ebdf32b9040d277f89595369b33
moriash <moria2002@gmail.com>
2019-12-31 15:07:55 +0200

    add ground

--- mineCraft.js
+++ mineCraft.js
@@ -11,17 +11,18 @@ mineCraft.mapArray = [
     [],
     []
 ];
-mineCraft.start = () =>{
+mineCraft.start = () => {
     mineCraft.createDiv();
+    mineCraft.createGround();
 }
-mineCraft.createDiv = () =>{
+mineCraft.createDiv = () => {
     for (let i = 0; i < mineCraft.mapArray.length; i++) {
         let row = document.createElement('div');
         row.setAttribute('class', 'row');
         document.querySelector('.map').appendChild(row);
         for (let j = 0; j < 10; j++) {
             // console.log(mapArray);
-    
+
             let block = document.createElement('div');
             block.setAttribute('class', 'block');
             mineCraft.mapArray[i].push(j);
@@ -29,7 +30,14 @@ mineCraft.createDiv = () =>{
             row.appendChild(block);
         }
     }
-
 }
+mineCraft.createGround = () => {
+    for (let i = 7; i < 10; i++) {
+        for (let j = 0; j < mineCraft.mapArray[i].length; j++) {
+            document.querySelector(`#c${i}_${j}`).style.backgroundImage = 'url("./images/dirt.jpg")';
+        }
+    }
+}
+
 
 mineCraft.start();
