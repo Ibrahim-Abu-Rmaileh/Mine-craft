d530cd72032254ac95f959baac345a394394148c
moriash <moria2002@gmail.com>
2019-12-31 14:48:27 +0200

    add obj

--- mineCraft.js
+++ mineCraft.js
@@ -1,4 +1,5 @@
-var mapArray = [
+const mineCraft = {};
+mineCraft.mapArray = [
     [],
     [],
     [],
@@ -10,24 +11,25 @@ var mapArray = [
     [],
     []
 ];
-
-for (let i = 0; i < mapArray.length; i++) {
-    let row = document.createElement('div');
-    row.setAttribute('class', 'row');
-    document.querySelector('.map').appendChild(row);
-    for (let j = 0; j < 10; j++) {
-        // console.log(mapArray);
-
-        let block = document.createElement('div');
-        block.setAttribute('class', 'block');
-        mapArray[i].push(j);
-        block.setAttribute('id', "c" + i + "_" + j);
-        row.appendChild(block);
-
-
+mineCraft.start = () =>{
+    mineCraft.createDiv();
+}
+mineCraft.createDiv = () =>{
+    for (let i = 0; i < mineCraft.mapArray.length; i++) {
+        let row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        document.querySelector('.map').appendChild(row);
+        for (let j = 0; j < 10; j++) {
+            // console.log(mapArray);
+    
+            let block = document.createElement('div');
+            block.setAttribute('class', 'block');
+            mineCraft.mapArray[i].push(j);
+            block.setAttribute('id', "c" + i + "_" + j);
+            row.appendChild(block);
+        }
     }
 
-
-
-
 }
+
+mineCraft.start();
