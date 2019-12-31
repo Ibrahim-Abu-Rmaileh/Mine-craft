6dbd1309937d36ef1010552d94e94d1549ad3cd0
litalz <litalbdbd@gmail.com>
2019-12-31 15:11:09 +0200

    test

--- mineCraft.js
+++ mineCraft.js
@@ -11,17 +11,17 @@ mineCraft.mapArray = [
     [],
     []
 ];
-mineCraft.start = () =>{
+mineCraft.start = () => {
     mineCraft.createDiv();
 }
-mineCraft.createDiv = () =>{
+mineCraft.createDiv = () => {
     for (let i = 0; i < mineCraft.mapArray.length; i++) {
         let row = document.createElement('div');
         row.setAttribute('class', 'row');
         document.querySelector('.map').appendChild(row);
         for (let j = 0; j < 10; j++) {
             // console.log(mapArray);
-    
+
             let block = document.createElement('div');
             block.setAttribute('class', 'block');
             mineCraft.mapArray[i].push(j);
